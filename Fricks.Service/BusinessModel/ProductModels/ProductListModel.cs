d8b52786351c5938fa8921b90479c5808ed68e97
Dang Gia Duc <giaducdang@gmail.com>
2024-10-10 23:36:23 +0700

    fix: get list products

--- Fricks.Service/BusinessModel/ProductModels/ProductListModel.cs
+++ Fricks.Service/BusinessModel/ProductModels/ProductListModel.cs
@@ -15,8 +15,11 @@ namespace Fricks.Service.BusinessModel.ProductModels
         public string? Sku { get; set; }
         public string? Name { get; set; }
         public string? Image { get; set; }
+        public int? CategoryId { get; set; }
         public string? CategoryName { get; set; }
+        public int? BrandId { get; set; }
         public string? BrandName { get; set; }
+        public int? StoreId { get; set; }
         public string? StoreName { get; set; }
         public string? Description { get; set; }
         public int? Quantity { get; set; }
