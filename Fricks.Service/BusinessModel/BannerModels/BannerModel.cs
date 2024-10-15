d1201ed49eec39f7fc8786070c9ae82cb25f46af
Pham Ngoc Bao <1005hanhphuc@gmail.com>
2024-10-15 23:50:12 +0700

    feat: CRUD Banner

---
+++ Fricks.Service/BusinessModel/BannerModels/BannerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fricks.Service.BusinessModel.BannerModels
+{
+    public class BannerModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Image { get; set; }
+
+        public int Index { get; set; }
+    }
+}
